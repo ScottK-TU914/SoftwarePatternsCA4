51859fdcd0cd6fca4f41ff4b56a069c8d1454cbb
ScottK-TU914 <C21392536@mytudublin.ie>
2025-04-16 15:02:37 +0100

    Update: Book Class

--- bookshop/src/main/java/com/bookshop/bookshop/models/Book.java
+++ bookshop/src/main/java/com/bookshop/bookshop/models/Book.java
@@ -1,5 +1,46 @@
 package com.bookshop.bookshop.models;
 
+import jakarta.persistence.*;
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private Double price;
+    private int stock;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
 
 }
