608755fe2abcf1749128e3222f2a3d173206c494
ScottK-TU914 <C21392536@mytudublin.ie>
2025-04-18 02:19:25 +0100

    Update: Added Sample Book
    - Set up a CommandLineRunner to add a book to the database
    - Previously created home.html now displays this created book in
    appropriate format.
    - Updated book.java to support image, and solved a minor issue in
    regards to ID not being found.

--- bookshop/src/main/java/com/bookshop/bookshop/BookshopApplication.java
+++ bookshop/src/main/java/com/bookshop/bookshop/BookshopApplication.java
@@ -1,13 +1,35 @@
 package com.bookshop.bookshop;
 
+import com.bookshop.bookshop.models.Book;
+import com.bookshop.bookshop.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner addSampleBook(BookRepository bookRepo) {
+        return args -> {
+            if (bookRepo.count() == 0) {
+                bookRepo.save(new Book(
+                        "The Hobbit",
+                        "J.R.R. Tolkien",
+                        "HarperCollins",
+                        "Fantasy",
+                        "9780007458424",
+                        14.99,
+                        "hobbit.jpg",
+                        10
+                ));
+                System.out.println("Book added: The Hobbit");
+            }
+        };
+    }
 }
--- bookshop/src/main/java/com/bookshop/bookshop/controllers/HomeController.java
+++ bookshop/src/main/java/com/bookshop/bookshop/controllers/HomeController.java
@@ -1,13 +1,28 @@
 package com.bookshop.bookshop.controllers;
 
+import com.bookshop.bookshop.models.Book;
+import com.bookshop.bookshop.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public HomeController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
         return "home";
     }
 }
