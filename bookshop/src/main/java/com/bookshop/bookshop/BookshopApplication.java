a6db0cbf83d7d9df4a1559cb5e77c61669266496
ScottK-TU914 <C21392536@mytudublin.ie>
2025-04-19 18:11:50 +0100

    Update: Books added to Database

--- bookshop/src/main/java/com/bookshop/bookshop/BookshopApplication.java
+++ bookshop/src/main/java/com/bookshop/bookshop/BookshopApplication.java
@@ -28,7 +28,60 @@ public class BookshopApplication {
                         "hobbit.jpg",
                         10
                 ));
-                System.out.println("Book added: The Hobbit");
+
+                bookRepo.save(new Book(
+                        "Harry Potter and the Philosopher's Stone",
+                        "J.K Rowling",
+                        "Bloomsbury",
+                        "Fantasy",
+                        "9780747532699",
+                        19.50,
+                        "potter.jpg",
+                        7
+                ));
+
+                bookRepo.save(new Book(
+                        "Murder on the Orient Express",
+                        "Agatha Christie",
+                        "William Morrow",
+                        "Crime",
+                        "9780062073501",
+                        19.99,
+                        "orient.jpg",
+                        23
+                ));
+                bookRepo.save(new Book(
+                        "The Hunger Games",
+                        "Suzanne Collins",
+                        "Scholastic",
+                        "Fiction",
+                        "9781407132075",
+                        30.99,
+                        "hungergames.jpg",
+                        30
+                ));
+                bookRepo.save(new Book(
+                        "1984",
+                        "George Orwell",
+                        "Penguin",
+                        "Fiction",
+                        "9780141036144",
+                        14.99,
+                        "1984.jpg",
+                        2
+                ));
+                bookRepo.save(new Book(
+                        "Of Mice and Men",
+                        "John Steinbeck",
+                        "Penguin",
+                        "Fiction",
+                        "9780141023571",
+                        10.00,
+                        "ofmiceandmen.jpg",
+                        25
+                ));
+
+                System.out.println("Books added: The Hobbit, Harry Potter and the Philosopher's Stone, Murder on the Orient Express, The Hunger Games, 1984, Of Mice and Men  ");
             }
         };
     }
