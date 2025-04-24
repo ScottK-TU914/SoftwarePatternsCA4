cbf71ed7c5107b2453998accd2613bd79491bb3d
ScottK-TU914 <C21392536@mytudublin.ie>
2025-04-24 17:55:54 +0100

    Updated: Added Comments

--- bookshop/src/main/java/com/bookshop/bookshop/command/CartOrderCommand.java
+++ bookshop/src/main/java/com/bookshop/bookshop/command/CartOrderCommand.java
@@ -1,13 +1,13 @@
 package com.bookshop.bookshop.command;
 
 import com.bookshop.bookshop.services.OrderService;
-
+//this class features the Command pattern.
 public class CartOrderCommand implements OrderCommand {
 
-    private final OrderService orderService;
-    private final Long bookId;
-    private final String username;
-    private final int quantity;
+    private final OrderService orderService; //service that will carry out the order
+    private final Long bookId; //id of book being ordered
+    private final String username; //username of buyer 
+    private final int quantity; //amoutn of books being bought
 
     public CartOrderCommand(OrderService orderService, Long bookId, String username, int quantity) {
         this.orderService = orderService;
@@ -18,6 +18,7 @@ public class CartOrderCommand implements OrderCommand {
 
     @Override
     public void execute() {
+        //calls the service method with the given details
         orderService.placeOrder(bookId, username, quantity);
     }
 }
